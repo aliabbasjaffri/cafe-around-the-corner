68b6d20324a183cd8b9d7a295d381d3fbb9ccd13
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2021-01-07 22:40:13 +0100

    Added central theme file with theme colors in config

---
+++ src/config/theme.ts
@@ -0,0 +1,35 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const themeColors = {
+  primary: {
+    main: '#90caf9',
+    light: '#a6d4fa',
+    dark: '#648dae',
+    contrastText: '#a6d4fa'
+  },
+  secondary: {
+    main: '#2196f3',
+    light: '#64b5f6',
+    dark: '#1976d2',
+    contrastText: '#64b5f6'
+  }
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: themeColors.primary.main,
+      light: themeColors.primary.light,
+      dark: themeColors.primary.dark,
+      contrastText: themeColors.primary.contrastText
+    },
+    secondary: {
+      main: themeColors.secondary.main,
+      light: themeColors.secondary.light,
+      dark: themeColors.secondary.dark,
+      contrastText: themeColors.secondary.contrastText
+    }
+  }
+});
+
+export default { theme };
