b26cf13713c678f74280d1c0a2e8498b0300f4c8
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2021-02-19 12:40:47 +0100

    Updated Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from '@material-ui/core';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <AppBar position="relative">
+      <Toolbar />
+    </AppBar>
+  );
+};
+
+export default Header;
